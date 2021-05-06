dab1ead77dd666c81cc72319f5575f4e37102e45
Inryu <dellintel17@naver.com>
2021-05-06 15:49:40 +0900

    YACC 실습1

---
+++ YACC 실습/실습1/Lab1/Lab1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+extern int yyparse();
+
+int main()
+{
+
+	printf("==================\n");
+	printf("team2\n");
+	printf("1871026 신인류\n");
+	printf("1871040 이유정\n");
+	printf("1876136 박가현\n");
+	printf("==================\n");
+	
+	return yyparse();
+}
